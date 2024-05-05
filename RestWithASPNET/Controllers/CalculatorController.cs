a8180a33b5dfcf53089f192418fbf5c9110b71cd
Camila-Isabela <camila_isabela_lins@hotmail.com>
2024-05-05 19:00:38 -0300

    Refactor Code

--- RestWithASPNET/Controllers/CalculatorController.cs
+++ RestWithASPNET/Controllers/CalculatorController.cs
@@ -15,32 +15,12 @@ public class CalculatorController :ControllerBase
     [HttpGet("sum/{firstNumber}/{secondNumber}")]
     public IActionResult Get(string firstNumber, string secondNumber)
     {
-        if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        if(decimal.TryParse(firstNumber, out decimal first) && decimal.TryParse(secondNumber, out decimal second))
         {
-            var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+            var sum = first + second;
             return Ok(sum.ToString());
         }
         return BadRequest("Invalid Input");
-    }
-
-    private bool IsNumeric(string strNumber)
-    {
-        double number;
-        bool isNumber = double.TryParse(
-            strNumber, 
-            System.Globalization.NumberStyles.Any, 
-            System.Globalization.NumberFormatInfo.InvariantInfo, 
-            out number);
-        return isNumber;
-    }
-    private decimal ConvertToDecimal(string strNumber)
-    {
-        decimal decimalValue;
-        if(decimal.TryParse(strNumber, out decimalValue))
-        {
-            return decimalValue;
-        }
-        throw new ArgumentException("Invalid Input");
     }
 
 }

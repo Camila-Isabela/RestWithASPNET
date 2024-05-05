97278c52cb795a98d1d25b1395fea083cbb2c11c
Camila-Isabela <camila_isabela_lins@hotmail.com>
2024-05-05 18:32:16 -0300

    Create initial logic

--- RestWithASPNET/Controllers/CalculatorController.cs
+++ RestWithASPNET/Controllers/CalculatorController.cs
@@ -13,8 +13,23 @@ public class CalculatorController :ControllerBase
     }
 
     [HttpGet("sum/{firstNumber}/{secondNumber}")]
-    public IActionResult Get(string firstNumber, string secondnumber)
+    public IActionResult Get(string firstNumber, string secondNumber)
     {
+        if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+            return Ok(sum.ToString());
+        }
         return BadRequest("Invalid Input");
+    }
+
+    private int ConvertToDecimal(string firstNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    private bool IsNumeric(string firstNumber)
+    {
+        throw new NotImplementedException();
     }
 }
